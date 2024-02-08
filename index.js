4fbcd4498f6ad739fd60009cc39ba115d8ddd550
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-02-08 19:46:39 +0900

    lint

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { SAVE_DIR, FILE_EXT, DURATION_MS, TIME_FORMAT } = process.env
 
-const handler = _ => setInterval(_ => {
-  const fileName = DateTime.local().toFormat(TIME_FORMAT)
-  const saveDir = SAVE_DIR.replace(/[\/\\]$/, '')
-  const filePath = `${saveDir}/${fileName}.${FILE_EXT}`
+const handler = _ =>
+    setInterval(_ => {
+        const dateNow = DateTime.local()
+        const fileName = dateNow.toFormat(TIME_FORMAT)
+        const saveDir = SAVE_DIR.replace(/[\/\\]$/, '')
+        const filePath = `${saveDir}/${fileName}.${FILE_EXT}`
 
-  screenshot(filePath, (error, _complete) => {
-    if (error) {
-      console.log('Screenshot failed', error)
-    } else {
-      console.log('saved: ' + filePath)
-    }
-  })
-}, DURATION_MS)
+        screenshot(filePath, (error, _complete) => {
+            if (error) {
+                console.log('Screenshot failed', error)
+            } else {
+                console.log('Saved: ' + filePath)
+            }
+        })
+    }, DURATION_MS)
 
 handler()
