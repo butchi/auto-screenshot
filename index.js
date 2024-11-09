aa883ad7b27125957835561a01082500047e08a7
IWABUCHI Yuki <butchiyu+com-github@gmail.com>
2024-11-09 18:58:14 +0900

    再実装

--- index.js
+++ index.js
@@ -1,27 +1,76 @@
-import screenshot from 'desktop-screenshot'
+import screenshot from 'screenshot-desktop'
+import sharp from 'sharp'
 import { DateTime } from 'luxon'
 
 import dotenv from 'dotenv'
 
 dotenv.config()
-const { SAVE_DIR, FILE_EXT, DURATION_MS, TIME_FORMAT } = process.env
+const { SAVE_DIR, TIME_FORMAT } = process.env
 
-const handler = _ =>
+const handler = async _ => {
+    const saveDir = SAVE_DIR.replace(/[\/\\]$/, '')
+
+    let millisecond
+
+    millisecond = 60 * 1000
+
+    // stream thumbnail screenshot
     setInterval(_ => {
         const dateNow = DateTime.local()
         const fileName = dateNow.toFormat(TIME_FORMAT)
-        const saveDir = SAVE_DIR.replace(/[\/\\]$/, '')
-        const filePath = `${saveDir}/${fileName}.${FILE_EXT}`
-        const nextTime = dateNow.plus({ millisecond: DURATION_MS })
+        const streamImgPath = `${saveDir}/stream/${fileName}.jpg`
 
-        screenshot(filePath, (error, _complete) => {
-            if (error) {
-                console.log('Screenshot failed', error)
-            } else {
-                console.log('Saved: ' + filePath)
-                console.log('Next: ' + nextTime.toFormat('HH:mm:ss'))
-            }
-        })
-    }, DURATION_MS)
+        screenshot()
+            .then(img => {
+                sharp(img)
+                    .resize({ height: 720 })
+                    .jpeg({ quality: 65 })
+                    .toFile(streamImgPath, (err, info) => {
+                        if (err) {
+                            console.log('Screenshot failed', err)
+                        } else {
+                            console.log('Saved: ' + streamImgPath)
+                        }
+
+                        // if (info) {
+                        //     console.info('Screenshot info', info)
+                        // }
+                    })
+            })
+            .catch(err => {
+                console.log('Screenshot failed', err)
+            })
+    }, millisecond)
+
+    millisecond = 15 * 60 * 1000
+
+    // full image screenshot
+    setInterval(_ => {
+        const dateNow = DateTime.local()
+        const fileName = dateNow.toFormat(TIME_FORMAT)
+        const fullImgPath = `${saveDir}/${fileName}.png`
+        const nextTime = dateNow.plus({ millisecond })
+
+        screenshot()
+            .then(img => {
+                sharp(img).toFile(fullImgPath, (err, info) => {
+                    if (err) {
+                        console.log('Screenshot failed', err)
+                    } else {
+                        console.log('Saved: ' + fullImgPath)
+                    }
+
+                    // if (info) {
+                    //     console.info('Screenshot info', info)
+                    // }
+
+                    console.log('Next: ' + nextTime.toFormat('HH:mm:ss'))
+                })
+            })
+            .catch(err => {
+                console.log('Screenshot failed', err)
+            })
+    }, millisecond)
+}
 
 handler()
